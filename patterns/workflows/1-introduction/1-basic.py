0d6e8d0104ded27df2a7bf3b6771569f5c6acf4d
KristofferSimlin <Zimlin32@gmail.com>
2025-11-11 23:52:52 +0100

    Update .env.example and Python scripts to enhance functionality and clarity
    - Clear the OPENAI_API_KEY in .env.example for security.
    - Load environment variables in 1-basic.py for better configuration management.
    - Update system prompt in 1-basic.py to reflect the assistant's name.
    - Change user prompt in 3-tools.py to inquire about the weather in Bålsta.

--- patterns/workflows/1-introduction/1-basic.py
+++ patterns/workflows/1-introduction/1-basic.py
@@ -1,5 +1,6 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 from openai import OpenAI
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
@@ -8,10 +9,10 @@ client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 completion = client.chat.completions.create(
     model="gpt-4o",
     messages=[
-        {"role": "system", "content": "You're a helpful assistant."},
+        {"role": "system", "content": "You're name is SERA and your a mechanical assistant."},
         {
             "role": "user",
-            "content": "Write a limerick about the Python programming language.",
+            "content": "hi.",
         },
     ],
 )
